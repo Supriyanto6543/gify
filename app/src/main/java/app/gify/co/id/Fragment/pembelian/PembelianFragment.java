ab2fc80096b5556194855eb720a9196efeb7e022
Rifqi Naufal <rifqigoku2122@gmail.com>
2020-03-19 21:23:48 +0700

    Rifqi Ganteng V8

--- app/src/main/java/app/gify/co/id/Fragment/pembelian/PembelianFragment.java
+++ app/src/main/java/app/gify/co/id/Fragment/pembelian/PembelianFragment.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import app.gify.co.id.Fragment.home.HomeFragment;
 import app.gify.co.id.R;
 import app.gify.co.id.activity.MainActivity;
 import app.gify.co.id.activity.Pembelian;
@@ -19,10 +23,20 @@ import app.gify.co.id.activity.Pembelian;
 public class PembelianFragment extends Fragment {
 
     ImageView navView;
+    Button cariKadoPembelian;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pembelian, container, false);
+        cariKadoPembelian = view.findViewById(R.id.cariKadoPembelian);
+        cariKadoPembelian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = null;
+                fragment = new HomeFragment();
+                replaceFragment(fragment);
+            }
+        });
 
         navView = view.findViewById(R.id.backDetailKado);
         navView.setOnClickListener(new View.OnClickListener() {
@@ -32,5 +46,13 @@ public class PembelianFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    public void replaceFragment(Fragment someFragment) {
+        assert getFragmentManager() != null;
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame, someFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
--- app/src/main/java/app/gify/co/id/activity/Pengaturan.java
+++ app/src/main/java/app/gify/co/id/activity/Pengaturan.java
@@ -1,5 +1,6 @@
 package app.gify.co.id.activity;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
